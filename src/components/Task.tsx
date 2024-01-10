dafec5b96664b6543c1b5e85726962c1b49c2c1b
Joel Thompson <joel.thompson@circleci.com>
2024-01-10 07:58:15 -0800

    working add remove

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,23 +1,45 @@
 import { Draggable } from "@hello-pangea/dnd";
-import { Box } from "@mui/material";
+import { Button, Stack, useTheme } from "@mui/material";
+import DragHandleIcon from "@mui/icons-material/DragHandle";
 
 export interface TaskType {
   id: string;
   content: string;
 }
 
-export const Task = ({ task, index }: { task: TaskType; index: number }) => {
+export const Task = ({
+  task,
+  index,
+  removeTask,
+}: {
+  task: TaskType;
+  index: number;
+  removeTask: (id: string) => void;
+}) => {
+  const theme = useTheme();
   return (
     <Draggable draggableId={task.id} index={index}>
       {(provided) => (
-        <Box
+        <Stack
+          alignItems={"center"}
+          flexBasis={"content"}
+          direction={"row"}
           sx={{ ":hover": { cursor: "grab" } }}
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
+          <DragHandleIcon sx={{ marginRight: theme.spacing(1) }} />
           {task.content}
-        </Box>
+          <Button
+            color="secondary"
+            size="small"
+            onClick={() => removeTask(task.id)}
+            sx={{ marginLeft: theme.spacing(1) }}
+          >
+            Remove
+          </Button>
+        </Stack>
       )}
     </Draggable>
   );
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,16 +2,18 @@ import { Droppable } from "@hello-pangea/dnd";
 import { Task, TaskType } from "./Task";
 import { Box } from "@mui/material";
 
-export const TaskList = ({ tasks }: { tasks: TaskType[] }) => {
+interface Props {
+  tasks: TaskType[];
+  removeTask: (id: string) => void;
+}
+
+export const TaskList = ({ tasks, removeTask }: Props) => {
   return (
     <Droppable droppableId="tasks">
       {(provided) => (
-        <Box
-          {...provided.droppableProps}
-          ref={provided.innerRef}
-        >
+        <Box {...provided.droppableProps} ref={provided.innerRef}>
           {tasks.map((task, index) => (
-            <Task key={task.id} task={task} index={index} />
+            <Task key={task.id} task={task} index={index} removeTask={removeTask} />
           ))}
           {provided.placeholder}
         </Box>
